957d5a64acc37b9928c97d8a3cc904f39b42813c
Ryan Hartlage <ryanplusplus@gmail.com>
2019-12-15 15:32:40 -0500

    Interrupt-driven display update

--- src/app/w1209/thermistor.c
+++ src/app/w1209/thermistor.c
@@ -17,12 +17,9 @@
 
 /* Look-up table for raw ADC counts for every degree between
    20 F and 110 F */
-const unsigned int raw_adc_counts[] = {
+static const unsigned int raw_adc_counts[] = {
   58787, 58507, 58227, 57947, 57667, 57387, 57107, 56827, 56547, 56267, 55987, 55707, 55427, 55147, 54867, 54587, 54307, 54027, 53747, 53467, 53187, 52907, 52627, 52347, 52067, 51787, 51507, 51227, 50947, 50667, 50387, 50107, 49827, 49547, 49267, 48987, 48707, 48427, 48147, 47867, 47587, 47307, 47027, 46747, 46467, 46187, 45907, 45627, 45347, 45067, 44787, 44507, 44227, 43947, 43667, 43387, 43107, 42827, 42547, 42267, 41987, 41707, 41427, 41147, 40867, 40587, 40307, 40027, 39747, 39467, 39187, 38907, 38627, 38347, 38067, 37787, 37507, 37227, 36947, 36667, 36387, 36107, 35827, 35547, 35267, 34987, 34707, 34427, 34147, 33867, 33587
 };
-
-static volatile tiny_adc_counts_t counts;
-static int temperature;
 
 typedef struct {
   tiny_event_t thermistor_read_event;
@@ -40,21 +37,7 @@ i_tiny_event_t* thermistor_read_event(void) {
   return &thermistor.thermistor_read_event.interface;
 }
 
-void thermistor_read(void) {
-  counts = tiny_adc_group_read(thermistor.adc_group, 6);
-  if(counts > ADC_COUNTS_MIN) {
-    temperature = get_temperature();
-    tiny_event_publish(&thermistor.thermistor_read_event, &temperature);
-    counts++;
-  }
-  else {
-    counts++;
-  }
-
-  
-}
-
-int get_temperature(void) {
+static int get_temperature(tiny_adc_counts_t counts) {
   unsigned char right_bound_temp_index = ADC_RAW_TABLE_SIZE;
   unsigned char left_bound_temp_index = 0;
 
@@ -72,4 +55,17 @@ int get_temperature(void) {
 
   // Estimate low if between two values
   return ADC_RAW_TABLE_BASE_TEMP + left_bound_temp_index;
+}
+
+void thermistor_read(void) {
+  tiny_adc_counts_t counts = tiny_adc_group_read(thermistor.adc_group, 6);
+
+  if(counts > ADC_COUNTS_MIN) {
+    int temperature = get_temperature(counts);
+    tiny_event_publish(&thermistor.thermistor_read_event, &temperature);
+    counts++;
+  }
+  else {
+    counts++;
+  }
 }
--- src/device/seven_segment_display.c
+++ src/device/seven_segment_display.c
@@ -3,9 +3,10 @@
  * @brief
  */
 
+#include <stddef.h>
+#include <string.h>
 #include "stm8s_gpio.h"
 #include "seven_segment_display.h"
-#include <string.h>
 
 #define init_output(_port, _pin)   \
   GPIO##_port->DDR |= (1 << _pin); \
@@ -17,77 +18,71 @@
   else                                    \
     GPIO##_port->ODR &= ~(1 << _pin)
 
-static void delay(void) {
-  int i;
-  for(i = 1; i <= 10000; i++) {
-  }
-}
+static tiny_event_subscription_t interrupt_subscription;
 
-void seven_segment_display_init(void) {
-  // Turns everything on
-  init_output(D, 5); // A
-  init_output(A, 2); // B
-  init_output(C, 7); // C
-  init_output(D, 3); // D
-  init_output(D, 1); // E
-  init_output(A, 1); // F
-  init_output(C, 6); // G
-  init_output(D, 2); // P
-
-  init_output(D, 4); // 1
-  init_output(B, 5); // 2
-  init_output(B, 4); // 3
-
-  // 1 is on, 0 is off
-  write_output(D, 5, 1); // A
-  write_output(A, 2, 1); // B
-  write_output(C, 7, 1); // C
-  write_output(D, 3, 1); // D
-  write_output(D, 1, 1); // E
-  write_output(A, 1, 1); // F
-  write_output(C, 6, 1); // G
-  write_output(D, 2, 1); // P
-
-  // 1 is off, 0 is on
-  write_output(D, 4, 0); // 1
-  write_output(B, 5, 0); // 2
-  write_output(B, 4, 0); // 3
-}
-
-void seven_segment_display_sect_on(int section) {
+static void digit_on(int section) {
   switch(section) {
-    case 1:
+    case 0:
       write_output(D, 4, 0);
       break;
-    case 2:
+
+    case 1:
       write_output(B, 5, 0);
       break;
-    case 3:
+
+    case 2:
       write_output(B, 4, 0);
       break;
+
     default:
       break;
   }
 }
 
-void seven_segment_display_sect_off(int section) {
+static void digit_off(int section) {
   switch(section) {
-    case 1:
+    case 0:
       write_output(D, 4, 1);
       break;
-    case 2:
+
+    case 1:
       write_output(B, 5, 1);
       break;
-    case 3:
+
+    case 2:
       write_output(B, 4, 1);
       break;
+
     default:
       break;
   }
 }
 
-void seven_segment_display_number(char number) {
-  switch(number) {
+static void display_character(char character) {
+  switch(character) {
+    case 0:
+    case ' ':
+      write_output(D, 5, 0); // A
+      write_output(A, 2, 0); // B
+      write_output(C, 7, 0); // C
+      write_output(D, 3, 0); // D
+      write_output(D, 1, 0); // E
+      write_output(A, 1, 0); // F
+      write_output(C, 6, 0); // G
+      write_output(D, 2, 0); // P
+      break;
+
+    case '0':
+      write_output(D, 5, 1); // A
+      write_output(A, 2, 1); // B
+      write_output(C, 7, 1); // C
+      write_output(D, 3, 1); // D
+      write_output(D, 1, 1); // E
+      write_output(A, 1, 1); // F
+      write_output(C, 6, 0); // G
+      write_output(D, 2, 0); // P
+      break;
+
     case '1':
       write_output(D, 5, 0); // A
       write_output(A, 2, 1); // B
@@ -109,7 +104,7 @@ void seven_segment_display_number(char number) {
       write_output(C, 6, 1); // G
       write_output(D, 2, 0); // P
       break;
-    
+
     case '3':
       write_output(D, 5, 1); // A
       write_output(A, 2, 1); // B
@@ -189,57 +184,55 @@ void seven_segment_display_number(char number) {
 
     default:
       break;
-
   }
 }
 
+static uint8_t current_digit;
+static char display_string_buffer[3];
+static char display_string[3];
+
+static void do_stuff(void* context, const void* args) {
+  (void)context;
+  (void)args;
+
+  if(current_digit == 2) {
+    display_string[0] = display_string_buffer[0];
+    display_string[1] = display_string_buffer[1];
+    display_string[2] = display_string_buffer[2];
+  }
+
+  display_character(' ');
+  digit_off(current_digit);
+  current_digit = (current_digit + 1) % 3;
+  display_character(display_string[current_digit]);
+  digit_on(current_digit);
+}
+
+void seven_segment_display_init(i_tiny_event_t* interrupt) {
+  // Turns everything on
+  init_output(D, 5); // A
+  init_output(A, 2); // B
+  init_output(C, 7); // C
+  init_output(D, 3); // D
+  init_output(D, 1); // E
+  init_output(A, 1); // F
+  init_output(C, 6); // G
+  init_output(D, 2); // P
+
+  init_output(D, 4); // 1
+  init_output(B, 5); // 2
+  init_output(B, 4); // 3
+
+  digit_off(0);
+  digit_off(1);
+  digit_off(2);
+
+  tiny_event_subscription_init(&interrupt_subscription, NULL, do_stuff);
+  tiny_event_subscribe(interrupt, &interrupt_subscription);
+}
+
 void seven_segment_display_temp(char* temp_str) {
-
-  int length = strlen(temp_str);
-
-  if(length == 1) {
-    seven_segment_display_sect_off(1);
-    seven_segment_display_sect_off(2);
-    seven_segment_display_sect_on(3);
-    seven_segment_display_number(temp_str[0]);
-  }
-
-  if(length == 2) {
-    while(1) {
-      seven_segment_display_sect_off(1);
-      for(int i = 0; i < length; i++) {
-        if(i == 0) {
-          seven_segment_display_sect_on(2);
-          seven_segment_display_sect_off(3);
-        } else if (i == 1) {
-          seven_segment_display_sect_off(2);
-          seven_segment_display_sect_on(3);
-        }
-        seven_segment_display_number(temp_str[i]);
-        delay();
-      }
-    }
-  }
-
-  if(length == 3) {
-    while(1) {
-      for(int i = 0; i < length; i++) {
-        if(i == 0) {
-          seven_segment_display_sect_on(1);
-          seven_segment_display_sect_off(2);
-          seven_segment_display_sect_off(3);
-        } else if (i == 1) {
-          seven_segment_display_sect_off(1);
-          seven_segment_display_sect_on(2);
-          seven_segment_display_sect_off(3);
-        } else if (i == 2) {
-          seven_segment_display_sect_off(1);
-          seven_segment_display_sect_off(2);
-          seven_segment_display_sect_on(3);
-        }
-        seven_segment_display_number(temp_str[i]);
-        delay();
-      }
-    }
-  }
+  display_string_buffer[0] = temp_str[0];
+  display_string_buffer[1] = temp_str[1];
+  display_string_buffer[2] = temp_str[2];
 }
